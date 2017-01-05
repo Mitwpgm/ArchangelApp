ba65dc8a58dbd160f5c5606353717ca08afbf7bf
Mitw <mwon341@gmail.com>
2017-01-05 18:41:04 +1300

    Initial commit

--- ArchangelApp/Program.cs
+++ ArchangelApp/Program.cs
@@ -10,6 +10,41 @@ namespace ArchangelApp
     {
         static void Main(string[] args)
         {
+            string optionSelected;
+            string name;
+
+            Console.WriteLine("_______________________________________________________________");
+
+            Console.WriteLine("Hello! Please enter your name...");
+
+            Console.WriteLine("_______________________________________________________________");
+
+            name = Console.ReadLine();
+
+            Console.Write("Hello! Very happy to meet you "  + name + " :) \n");
+ 
+            Console.WriteLine("_______________________________________________________________");
+            Console.WriteLine("My name is Mitchell, would you like to be my friend?  y / n ");
+
+            Console.WriteLine("_______________________________________________________________");
+            optionSelected = Console.ReadLine();
+
+            if(optionSelected.ToLower() == "n" )
+            {
+                Console.WriteLine("It's OK, thank you for your time and I wish you all the very best!  ");
+                Console.WriteLine("God Bless You! :) ");
+            }           
+            else
+            {
+                Console.WriteLine("I am very happy and look forward to hear from you :)  ");
+                Console.WriteLine("You can contact me at: mwon341@gmail.com");
+            }
+
+            Console.WriteLine("\n\n_______________________________________________________________");
+            Console.WriteLine("\n Press any key to exit..");
+            Console.WriteLine("\n_______________________________________________________________");
+            Console.ReadKey();
+            
         }
     }
 }
